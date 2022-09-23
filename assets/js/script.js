b3a6ed6c25d40dc5d43db62284622a6c05c80341
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-09-22 20:32:49 -0400

    Updated sample

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,20 @@ btnSearch.on("click", function(e){
     var search = document.getElementById("search");
     var results = document.getElementById("searchResults");
     // Youtube API call
-    fetch('https://youtube.googleapis.com/youtube/v3/search?q='+ search.value + '&type=video&key=AIzaSyAgdR40xM9JWdT6pNZ88la8l8UpH9cAc3Y')
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data.items.length, data);
-        for (var i=0; i<data.items.length; i++){
-            var item = document.createElement("iframe");
-            console.log("https://www.youtube.com/watch?v=" +data.items[i].id.videoId);
-
-
-        }
-    })
+    // fetch('https://youtube.googleapis.com/youtube/v3/search?q='+ search.value + '&type=video&key=[Auth+Key]')
+    // .then(function(response){
+    //     return response.json();
+    // })
+    // .then(function(data){
+    //     console.log(data.items.length, data);
+    //     for (var i=0; i<data.items.length; i++){
+    //         var item = document.createElement("embed");
+    //         item.setAttribute("src", "https://www.youtube.com/embed/" + data.items[i].id.videoId);
+    //         // console.log("https://www.youtube.com/watch?v=" + data.items[i].id.videoId);
+    //         // results.appendChild(item);
+    //         console.log(item)
+    //     }
+    // })
     // Giphy API call
     fetch("https://api.giphy.com/v1/gifs/search?api_key=bwocb4KLlWPjMVn0GRDP3Dnzb0jsGyhW&q=" + search.value + "&limit=20&offset=0&rating=g&lang=en")
     .then(function(response){
@@ -24,7 +25,10 @@ btnSearch.on("click", function(e){
     })
     .then(function(data){
         for (i=0; i<data.data.length; i++){
+            var item = document.createElement("embed");
             console.log(data.data[i].embed_url);
+            item.setAttribute("src", data.data[i].embed_url);
+            results.appendChild(item);
         }
     })
 })
